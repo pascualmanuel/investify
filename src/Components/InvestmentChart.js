3ca36b870dfe43c55899aa1b66052e149f55c2d8
pascualmanuel <manuelnpascual@gmail.com>
2024-06-28 11:46:27 +0200

    Recharts

---
+++ src/Components/InvestmentChart.js
@@ -0,0 +1,17 @@
+// src/components/InvestmentChart.js
+import React from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+const InvestmentChart = ({ data }) => {
+  return <></>;
+};
+
+export default InvestmentChart;
