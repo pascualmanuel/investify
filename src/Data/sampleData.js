82a540a23d88c71a78af6f0365ec58ad186344c0
pascualmanuel <manuelnpascual@gmail.com>
2024-07-01 11:53:08 +0200

    ab testing datasample

--- src/Data/sampleData.js
+++ src/Data/sampleData.js
@@ -1,7 +1,18 @@
-export const sampleData = [
-  { name: "Enero", amount: 1000 },
-  { name: "Febrero", amount: 2000 },
-  { name: "Marzo", amount: 1500 },
-  { name: "Abril", amount: 3000 },
-  { name: "Mayo", amount: 4000 },
-];
+// // src/data/sampleData.js
+// export const sampleData = [
+//   { name: 'Enero', amount: 1000 },
+//   { name: 'Febrero', amount: 2000 },
+//   { name: 'Marzo', amount: 1500 },
+//   { name: 'Abril', amount: 3000 },
+//   { name: 'Mayo', amount: 4000 },
+// ];
+InverSim;
+FinSimulador;
+SimuInversión;
+WealthWise;
+Investify;
+FutureFortune;
+ProfitPilot;
+GrowthGuru;
+InvestQuest;
+SimulaFortuna;
