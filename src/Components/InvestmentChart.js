ca7ba8c28cee1d024267e4ad7c59772293aa8626
pascualmanuel <manuelnpascual@gmail.com>
2024-07-01 11:51:45 +0200

    installing investify

--- src/Components/InvestmentChart.js
+++ src/Components/InvestmentChart.js
@@ -9,7 +9,6 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
-// npm install recharts Recharts is not installing
 
 const InvestmentChart = ({ data }) => {
   return <></>;
---
+++ src/Data/sampleData.js
@@ -0,0 +1,7 @@
+export const sampleData = [
+  { name: "Enero", amount: 1000 },
+  { name: "Febrero", amount: 2000 },
+  { name: "Marzo", amount: 1500 },
+  { name: "Abril", amount: 3000 },
+  { name: "Mayo", amount: 4000 },
+];
