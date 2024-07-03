2cdd0832fe2e46a441769e42160a11975aa43ee4
pascualmanuel <manuelnpascual@gmail.com>
2024-07-03 11:55:14 +0200

    Data sample ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from "./logo.svg";
+// src/App.js
+import React, { useState } from "react";
+import InvestmentForm from "./components/InvestmentForm";
+import InvestmentList from "./components/InvestmentList";
+import InvestmentChart from "./components/InvestmentChart";
+import { sampleData } from "./data/sampleData";
+import "./styles/App.css";
 
 function App() {
-  return <div className=""></div>;
+  const [investments, setInvestments] = useState([]);
+
+  const addInvestment = (investment) => {
+    setInvestments([...investments, investment]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simulador de Inversiones</h1>
+    </div>
+  );
 }
 
 export default App;
