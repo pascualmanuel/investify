bd903fdc05e07e960c0b62a403944f670c524934
pascualmanuel <manuelnpascual@gmail.com>
2024-07-04 12:05:53 +0200

    Adding more data to test

--- src/Data/sampleData.js
+++ src/Data/sampleData.js
@@ -1,7 +1,10 @@
 export const sampleData = [
-  { name: "Enero", amount: 300 },
-  { name: "Febrero", amount: 850 },
+  { name: "Enero", amount: 390 },
+  { name: "Febrero", amount: 821 },
   { name: "Marzo", amount: 1500 },
   { name: "Abril", amount: 3500 },
-  { name: "Mayo", amount: 4923 },
+  { name: "Mayo", amount: 5200 },
+  { name: "Junio", amount: 5850 },
+  { name: "Julio", amount: 6000 },
+  { name: "Agosto", amount: 5200 },
 ];
