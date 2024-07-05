4ff87b26a7067288e05e548de691073c9a2566a7
pascualmanuel <manuelnpascual@gmail.com>
2024-07-05 12:07:07 +0200

    Test Data

--- src/Data/sampleData.js
+++ src/Data/sampleData.js
@@ -7,8 +7,8 @@ export const sampleData = [
   { name: "Junio", amount: 5850 },
   { name: "Julio", amount: 6000 },
   { name: "Agosto", amount: 5200 },
-  { name: "Septiembre", amount: 5200 },
-  { name: "Octubre", amount: 5200 },
-  { name: "Noviembre", amount: 5200 },
-  { name: "Diciembre", amount: 5200 },
+  { name: "Septiembre", amount: 4200 },
+  { name: "Octubre", amount: 7200 },
+  { name: "Noviembre", amount: 9200 },
+  { name: "Diciembre", amount: 10200 },
 ];
