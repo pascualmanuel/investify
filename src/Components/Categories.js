8eafffc5f6b893323ca64c7d845cb975b2ecf1f0
pascualmanuel <manuelnpascual@gmail.com>
2024-07-08 12:09:43 +0200

    Starting categories

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Categories() {
-  return <></>;
+  return (
+    <>
+      <div className="background-categories">
+        <div className="categories-container max-2000">
+          <div className="categories-left-container">
+            <p style={{ marginBottom: "15px" }}>¿Cómo podemos ayudarte hoy?</p>
+            <h2>
+              <span style={{ color: "#5d80ff" }}> Cuatro </span>
+              categorías, <br></br> todas las soluciones.
+            </h2>
+            <p style={{ marginTop: "10px" }}>
+              Encuentra a los Expertos más
+              <br className="br-right-p-categories" /> recomendados para tu
+              hogar, <br className="br-right-p-categories" /> clases o
+              tecnología.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Categories;
