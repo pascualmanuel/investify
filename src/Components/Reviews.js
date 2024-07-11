c82921ad785139f26b98acfc9407c2d87c219531
pascualmanuel <manuelnpascual@gmail.com>
2024-07-11 12:19:10 +0200

    reviews errors solved

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,11 +1,14 @@
-const reviewsData = [
-];
-const ReviewItem = ({ img, name, text }) => (
-    <></>
-);
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import FiveStars from "../Assets/five-stars.svg";
+
+const reviewsData = [];
+
+const ReviewItem = ({ img, name, text }) => <></>;
+
 function Reviews() {
-    <></>
-  );
+  return <></>;
 }
 
 export default Reviews;
