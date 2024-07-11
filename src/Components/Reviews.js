d71cad0cf7bd533ffb8886438321de8e301d1f2f
pascualmanuel <manuelnpascual@gmail.com>
2024-07-11 12:19:23 +0200

    reviews Item

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -5,7 +5,27 @@ import FiveStars from "../Assets/five-stars.svg";
 
 const reviewsData = [];
 
-const ReviewItem = ({ img, name, text }) => <></>;
+const ReviewItem = ({ img, name, text }) => (
+  <div className="item">
+    <div className="reviews-box">
+      <img
+        src={img}
+        alt="Avatar"
+        width={55}
+        height={55}
+        style={{ marginLeft: "15px", marginTop: "15px" }}
+      />
+      <h2 className="reviews-title">{name}</h2>
+      <p className="reviews-text">{text}</p>
+      <img
+        src={FiveStars}
+        alt="FiveStars"
+        height={20}
+        style={{ marginLeft: "20px" }}
+      />
+    </div>
+  </div>
+);
 
 function Reviews() {
   return <></>;
