27f6c4eb7bdea01585b21cd96c985d5662d01670
pascualmanuel <manuelnpascual@gmail.com>
2024-07-23 12:23:13 +0200

    Suspend Chart

--- src/Components/InvestmentChart.js
+++ src/Components/InvestmentChart.js
@@ -1,26 +1,26 @@
-// src/components/InvestmentChart.js
-import React from "react";
-import {
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-} from "recharts";
+// // src/components/InvestmentChart.js
+// import React from "react";
+// import {
+//   LineChart,
+//   Line,
+//   CartesianGrid,
+//   XAxis,
+//   YAxis,
+//   Tooltip,
+//   Legend,
+// } from "recharts";
 
-const InvestmentChart = ({ data }) => {
-  return (
-    <LineChart width={600} height={300} data={data}>
-      <Line type="monotone" dataKey="amount" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-    </LineChart>
-  );
-};
+// const InvestmentChart = ({ data }) => {
+//   return (
+//     <LineChart width={600} height={300} data={data}>
+//       <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+//       <CartesianGrid stroke="#ccc" />
+//       <XAxis dataKey="name" />
+//       <YAxis />
+//       <Tooltip />
+//       <Legend />
+//     </LineChart>
+//   );
+// };
 
-export default InvestmentChart;
+// export default InvestmentChart;
