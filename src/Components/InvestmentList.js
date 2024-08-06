6c79ee0d2da62b091f71cf02ff1ac286a3b3dec3
pascualmanuel <manuelnpascual@gmail.com>
2024-08-06 12:33:26 +0200

    InvestList changes

--- src/Components/InvestmentList.js
+++ src/Components/InvestmentList.js
@@ -2,15 +2,13 @@ import React from "react";
 
 const InvestmentList = ({ investments }) => {
   return (
-    <>
-      <ul>
-        {investments.map((investment, index) => (
-          <li key={index}>
-            {investment.type}: ${investment.amount.toFixed(2)}
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul>
+      {investments.map((investment, index) => (
+        <li key={index}>
+          {investment.type}: ${investment.amount.toFixed(2)}
+        </li>
+      ))}
+    </ul>
   );
 };
 
