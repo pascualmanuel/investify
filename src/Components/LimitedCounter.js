50469a0bb148d09cab3e40cbf1d4e87268db4a92
pascualmanuel <manuelnpascual@gmail.com>
2024-08-12 15:51:23 -0300

    Count logc

--- src/Components/LimitedCounter.js
+++ src/Components/LimitedCounter.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
 const LimitedCounter = ({ limit = 10 }) => {};
+const [count, setCount] = useState(0);
 
+const increment = () => {
+  if (count < limit) setCount(count + 1);
+};
 export default LimitedCounter;
