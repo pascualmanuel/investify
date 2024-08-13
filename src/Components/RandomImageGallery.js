a7a2ea1703d9f7856bcd323e634b3a0bc7e8938d
pascualmanuel <manuelnpascual@gmail.com>
2024-08-13 15:55:38 -0300

    Gallery - Useffect

--- src/Components/RandomImageGallery.js
+++ src/Components/RandomImageGallery.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 const RandomImageGallery = () => {
+  const [images, setImages] = useState([]);
 
-
-
+  useEffect(() => {
+    fetch('')
+      .then((response) => response.json())
+      .then((data) => setImages(data));
+  }, []);
 
 
 };
