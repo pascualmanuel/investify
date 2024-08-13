3adb5033a9c5259e20c0ba375da1ad2c71a3965c
pascualmanuel <manuelnpascual@gmail.com>
2024-08-13 15:55:49 -0300

    Gallery - Useffect api

--- src/Components/RandomImageGallery.js
+++ src/Components/RandomImageGallery.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const RandomImageGallery = () => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch('')
+    fetch("https://picsum.photos/v2/list?page=2&limit=4")
       .then((response) => response.json())
       .then((data) => setImages(data));
   }, []);
-
-
 };
 
 export default RandomImageGallery;
