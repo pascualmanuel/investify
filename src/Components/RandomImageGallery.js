544e5b44f1907dc4c21b2f0e85b6b5b6e17694db
pascualmanuel <manuelnpascual@gmail.com>
2024-08-13 15:56:17 -0300

    Gallery - Finish?

--- src/Components/RandomImageGallery.js
+++ src/Components/RandomImageGallery.js
@@ -9,7 +9,19 @@ const RandomImageGallery = () => {
       .then((data) => setImages(data));
   }, []);
 
-  return <div style={{ display: "flex", gap: "10px" }}></div>;
+  return (
+    <div style={{ display: "flex", gap: "10px" }}>
+      {images.map((image) => (
+        <img
+          key={image.id}
+          src={image.download_url}
+          alt={image.author}
+          width={100}
+          height={100}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default RandomImageGallery;
