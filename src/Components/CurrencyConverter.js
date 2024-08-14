9759ed764b27c369e77e7861fcccc359e0943a49
pascualmanuel <manuelnpascual@gmail.com>
2024-08-14 16:04:21 -0300

    CConverter - front

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -4,8 +4,11 @@ const CurrencyConverter = () => {
   const [amount, setAmount] = useState(0);
   const exchangeRate = 0.85; // 1 USD = 0.85 EUR
 
-
+  return (
+    <div>
+      <h2>Currency Converter</h2>
+    </div>
+  );
 };
 
 export default CurrencyConverter;
-
