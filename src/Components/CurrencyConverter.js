f3644362fe39c73aa30bdcfeee92e8217ed9a272
pascualmanuel <manuelnpascual@gmail.com>
2024-08-14 16:03:57 -0300

    CConverter - UseState

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 
-const CurrencyConverter = () => {};
+const CurrencyConverter = () => {
+  const [amount, setAmount] = useState(0);
+  const exchangeRate = 0.85; // 1 USD = 0.85 EUR
+
+
+};
 
 export default CurrencyConverter;
+
