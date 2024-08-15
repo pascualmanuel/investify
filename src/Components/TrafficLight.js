0e8354a1cd60dbeddbbdc09c8a4e3a97f1869837
pascualmanuel <manuelnpascual@gmail.com>
2024-08-15 16:11:50 -0300

    TLight - UseEffect error

--- src/Components/TrafficLight.js
+++ src/Components/TrafficLight.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 const TrafficLight = () => {
-
+  const [light, setLight] = useState('red');
 
   useEffect(() => {
-
+    const interval = setInterval(() => {
+      setLight((prevLight) =>
+        prevLight === 'red' ? 'yellow' : prevLight === 'yellow' ? 'green' : 'red'
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
 };
 
