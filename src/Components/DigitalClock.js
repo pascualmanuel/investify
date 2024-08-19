403ab593bb11cd8f63520a62b28dd3d4d44228cb
pascualmanuel <manuelnpascual@gmail.com>
2024-08-19 16:18:55 -0300

    interval go

--- src/Components/DigitalClock.js
+++ src/Components/DigitalClock.js
@@ -4,10 +4,13 @@ const DigitalClock = () => {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
 
   useEffect(() => {
-
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
   }, []);
 
-
+  return <></>;
 };
 
 export default DigitalClock;
