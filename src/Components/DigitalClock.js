ee6304ffc92c28654cfac189079812402e48cf47
pascualmanuel <manuelnpascual@gmail.com>
2024-08-19 16:18:31 -0300

    DigitalClock Useffect

--- src/Components/DigitalClock.js
+++ src/Components/DigitalClock.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-const DigitalClock = () => {};
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+
+  }, []);
+
+
+};
 
 export default DigitalClock;
