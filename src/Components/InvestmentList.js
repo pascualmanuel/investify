ac6d113a5ed95e665c97d5a4792f37276ab4e6a9
pascualmanuel <manuelnpascual@gmail.com>
2024-08-21 15:47:32 -0300

    Rolling back

--- src/Components/InvestmentList.js
+++ src/Components/InvestmentList.js
@@ -6,8 +6,8 @@ const InvestmentList = ({ investments }) => {
     <ul>
       {investments.map((investment, index) => (
         <li key={index}>
-          {/* {investment.crypto}: ${investment.amount.toFixed(2)} comprados el{" "} */}
-          {/* {investment.date} a ${investment.price.toFixed(2)} cada uno hola */}
+          {investment.crypto}: ${investment.amount.toFixed(2)} comprados el{" "}
+          {investment.date} a ${investment.price.toFixed(2)} cada uno hola
         </li>
       ))}
     </ul>
