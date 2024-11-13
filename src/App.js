2f419f3fd82639ff4d452b0cc4bd2bb80afed680
pascualmanuel <manuelnpascual@gmail.com>
2024-11-12 22:52:10 -0300

    app chatbot

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import RandomQuoteGenerator from "./Components/RandomQuote";
 import DigitalClock from "./Components/DigitalClock";
 import DogAgeCalculator from "./Components/DogAgeCalculator";
 import CurrencyChatBot from "./Components/CurrencyChatBot";
+import ChatBot from "./Components/ChatBot/ChatBot";
 import Friends from "./Components/Friends";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Home";
@@ -44,7 +45,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/friends" element={<Friends />} />
           <Route path="/scroll" element={<SmoothScroll />} />
-          <Route path="/chatbot" element={<CurrencyChatBot />} />
+          <Route path="/chatbot" element={<ChatBot />} />
         </Routes>
       </BrowserRouter>
     </>
