6e1e720ffe67874083aff59644b338600b6f7b1a
pascualmanuel <manuelnpascual@gmail.com>
2024-11-14 09:49:19 -0300

    Cleaning home

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,39 +18,6 @@ import SmoothScroll from "./Components/SmoothScroll";
 import ChatBot from "./Components/ChatBot/Chatbot";
 
 const Home = () => {
-  const [investments, setInvestments] = useState([]);
-  const [chartData, setChartData] = useState([]);
-
-  const addInvestment = async (investment) => {
-    try {
-      const formattedDate = investment.date.split("-").join("-");
-      console.log(formattedDate);
-
-      const response = await axios.get(
-        `https://api.bluelytics.com.ar/v2/historical?day=${formattedDate}`
-      );
-      console.log(response);
-      const price = response?.data.blue?.value_sell || 0; // Manejar caso si `current_price` no está presente
-
-      const newInvestment = {
-        ...investment,
-        price,
-      };
-      setInvestments([...investments, newInvestment]);
-    } catch (error) {
-      console.error("Error fetching the crypto data:", error.message);
-    }
-  };
-
-  useEffect(() => {
-    // Transformar los datos de inversiones en el formato adecuado para el gráfico
-    const data = investments.map((inv, index) => ({
-      name: inv.crypto,
-      amount: inv.amount * inv.price,
-    }));
-    setChartData(data);
-  }, [investments]);
-
   return (
     <>
       <div className="App">
