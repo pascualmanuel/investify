043bfc8e5a791b1466f93fa6f415c8122f73d79b
pascualmanuel <manuelnpascual@gmail.com>
2024-11-14 09:49:48 -0300

    Only necessary imports for home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import InvestmentForm from "./Components/InvestmentForm";
-import InvestmentList from "./Components/InvestmentList";
-import InvestmentChart from "./Components/InvestmentChart";
+import React from "react";
 import "./Styles/App.css";
-import ReactDOM from "react";
-import LimitedCounter from "./Components/LimitedCounter";
-import RandomImageGallery from "./Components/RandomImageGallery";
-import CurrencyConverter from "./Components/CurrencyConverter";
-import TrafficLight from "./Components/TrafficLight";
-import RandomQuoteGenerator from "./Components/RandomQuote";
-import DigitalClock from "./Components/DigitalClock";
-
-import ShoppingList from "./Components/ShippingList";
-import Friends from "./Components/Friends";
-import SmoothScroll from "./Components/SmoothScroll";
 import ChatBot from "./Components/ChatBot/Chatbot";
 
 const Home = () => {
