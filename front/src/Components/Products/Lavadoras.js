59d679426e621d2feea048b04ebdc75265cf50c2
pascualmanuel <manuelnpascual@gmail.com>
2025-01-09 12:25:24 -0300

    Lavadoras Front -  #01000

--- front/src/Components/Products/Lavadoras.js
+++ front/src/Components/Products/Lavadoras.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./Product.css";
+import Carousel from "react-bootstrap/Carousel";
 
-const Lavadoras = () => {
+function Lavadoras() {
   return (
-   <><div>Lavadoras</div></>
+    <>
+      <div className="product-hero-cont"></div>
+
+      <div className="product-cont-categories">
+        <h3 className="product-title-categories">
+          Encuentra tu lavadora o secadora perfecta
+        </h3>
+        <div className="product-box-cont-categories">
+          <div className="product-box-category bla"></div>
+        </div>
+      </div>
+      {/* { if (isTablet)(<h2>You have unread messages.</h2>)} */}
+    </>
   );
-};
+}
 
 export default Lavadoras;
