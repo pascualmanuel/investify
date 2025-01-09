a4b8de4e18c353d73637aef8bf48892cca4689dd
pascualmanuel <manuelnpascual@gmail.com>
2025-01-09 12:25:49 -0300

    Lavadoras Front -  #001100

--- front/src/Components/Products/Lavadoras.js
+++ front/src/Components/Products/Lavadoras.js
@@ -3,9 +3,36 @@ import "./Product.css";
 import Carousel from "react-bootstrap/Carousel";
 
 function Lavadoras() {
+
   return (
     <>
-      <div className="product-hero-cont"></div>
+      <div className="product-hero-cont">
+        <Carousel fade controls={false} indicators={false} interval={null}>
+          <Carousel.Item>
+            <div
+              className="hero1"
+              style={{
+                height: "600px",
+                backgroundImage: `url(${HeroLavadoras})`,
+              }}
+            ></div>
+            <Carousel.Caption>
+              <div className="product-hero-sub-cont">
+                <div className="product-hero-left">
+                  <div className="product-banner">
+                    <h3 className="product-banner-title">Lavadoras</h3>
+                    <p className="product-banner-subtitle">
+                      <span style={{ fontWeight: "200" }}>que superan</span>
+                      <br></br>
+                      tus expectativas
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
 
       <div className="product-cont-categories">
         <h3 className="product-title-categories">
@@ -16,6 +43,7 @@ function Lavadoras() {
         </div>
       </div>
       {/* { if (isTablet)(<h2>You have unread messages.</h2>)} */}
+      
     </>
   );
 }
