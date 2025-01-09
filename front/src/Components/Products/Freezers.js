03a3a69814591265aef7241bd13e16d98e1c5ed9
pascualmanuel <manuelnpascual@gmail.com>
2025-01-09 18:34:56 -0300

    Freezer Front -  #00820

--- front/src/Components/Products/Freezers.js
+++ front/src/Components/Products/Freezers.js
@@ -48,7 +48,28 @@ function Freezers() {
           Encuentra tu freezer perfecto
         </h3>
         <div className="product-box-cont-categories">
-         
+          <div className="product-box-category bla">
+            <img
+              src={ProductFreezer1}
+              className="product-category-img"
+              width={180}
+              alt="category-img"
+            />
+            <p className="product-name">Freezer Horizontal 99 lts</p>
+            <p className="product-subtitle">
+              Congela o refrigera tus alimentos con el <br></br> menor consumo
+              de energía posible.
+              <br></br> Hecho para tí.
+            </p>
+            <a
+              href={`https://www.paris.cl/search?q=Daewoo&prefn1=isMarketplace&prefv1=Paris`}
+              target="_blank"
+              className="cta-product"
+              style={{ marginLeft: "0px" }}
+            >
+              Ver en tienda
+            </a>
+          </div>
           <div className="product-box-category bla">
             <img
               src={ProductFreezer2}
