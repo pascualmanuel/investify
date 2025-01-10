9ebdc5eb9e4043dd50d0928ce92b68dec15f101d
pascualmanuel <manuelnpascual@gmail.com>
2025-01-10 12:29:07 -0300

    Microondas Imports -  #002100

--- front/src/Components/Products/Microondas.js
+++ front/src/Components/Products/Microondas.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from "react";
 import "./Product.css";
+import Carousel from "react-bootstrap/Carousel";
+import HeroMicroondasDesktop from "../../Assets/products/microondas/carousel-microondas-min.jpg";
+import HeroMicroondasMobile from "../../Assets/products/microondas/hero-microondas-2.jpg";
+import ProductMicro1 from "../../Assets/products/microondas/Productos/microondas-20lts-1.webp";
+import ProductMicro2 from "../../Assets/products/microondas/Productos/microondas-23lts-1.webp";
+import EmocionalMob1 from "../../Assets/products/microondas/emocional-mob-1.jpg";
+import EmocionalMob2 from "../../Assets/products/microondas/emocional-mob-2.jpg";
+import EmocionalMob3 from "../../Assets/products/microondas/emocional-mob-3.jpg";
+import FirstCarousel from "../../Assets/products/microondas/carousel-microondas-1.jpg";
+import SecondCarousel from "../../Assets/products/microondas/carousel-microondas-3.jpg";
+import ThirdCarousel from "../../Assets/products/microondas/carousel-microondas-2.jpg";
 
 function Microondas() {
   return (
     <>
-      <div className="product-hero-cont"></div>
+      <div className="product-hero-cont">
+
+      </div>
       <div className="product-cont-categories">
-        <div className="product-box-cont-categories"></div>
+        
+        <div className="product-box-cont-categories">
+          
+          
+        </div>
       </div>
 
       {/* { if (isTablet)(<h2>You have unread messages.</h2>)} */}
