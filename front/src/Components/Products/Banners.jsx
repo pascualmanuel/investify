3f2b9119d8f75c568582eb9448255071180221da
pascualmanuel <manuelnpascual@gmail.com>
2025-01-11 12:35:26 -0300

    Banners DigitalMicrowave -  #003000

--- front/src/Components/Products/Banners.jsx
+++ front/src/Components/Products/Banners.jsx
@@ -9,12 +9,35 @@ function Banners() {
     <>
       <section className="micro-banner-cont">
         <div className="micro-banner-left"></div>
+        <div className="micro-banner-right">
+          <p className="micro-banner-sub">MICROONDAS DIGITAL</p>
+          <h3 className="micro-banner-title">
+            tu comida <br className="br-500" /> de ayer,
+            <br className="br-500" /> es más &nbsp;
+            <br className="br-1376" />
+            rica hoy
+          </h3>
+          <p className="micro-banner-p">
+            10 niveles de potencia y diferentes <br className="br-500" />
+            opciones de
+            <br className="br-banner-micro" /> cocción predeterminadas.
+          </p>
+          <div className="cta-banner-cont">
+            <a href="" className="cta-tienda" style={{ marginLeft: "0px" }}>
+              Ver en tienda
+            </a>
+            <Link to={"/microondas"} className="cta-banner">
+              Más info
+            </Link>
+          </div>
+        </div>
       </section>
       <section className="micro-banner-cont-hpt"></section>
       <section className="micro-banner-cont-hpt-mob"></section>
 
       <section className="freezer-banner-cont">
         <div className="micro-banner-left" id="freezer-img"></div>
+       
       </section>
 
       <section className="daewoo-banner-cont"></section>
