633ee1f29632c79ac6ded8b9f791a493ad4139c4
pascualmanuel <manuelnpascual@gmail.com>
2025-01-11 12:35:02 -0300

    Banners Strucutre -  #003000

--- front/src/Components/Products/Banners.jsx
+++ front/src/Components/Products/Banners.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-const Banners = () => {
+function Banners() {
   return (
     <>
-      <div>Banners</div>
+      <section className="micro-banner-cont">
+        <div className="micro-banner-left"></div>
+      </section>
+      <section className="micro-banner-cont-hpt"></section>
+      <section className="micro-banner-cont-hpt-mob"></section>
+
+      <section className="freezer-banner-cont">
+        <div className="micro-banner-left" id="freezer-img"></div>
+      </section>
+
+      <section className="daewoo-banner-cont"></section>
+      <div className="pre-footer-back"></div>
     </>
   );
-};
+}
 
 export default Banners;
