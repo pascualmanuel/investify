1f44151dca4602b79cd878e0ede1dad2a52519b6
pascualmanuel <manuelnpascual@gmail.com>
2025-01-14 12:46:36 -0300

    Hornos Imports-  #006100

--- front/src/Components/Products/HornosElectricos.js
+++ front/src/Components/Products/HornosElectricos.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import "./Product.css";
+import Carousel from "react-bootstrap/Carousel";
+import HeroHornosDesktop from "../../Assets/products/hornos-electricos/hero-horno.jpg";
+import HeroHornosMobile from "../../Assets/products/hornos-electricos/hero-horno-2.jpg";
+import FirstCarousel from "../../Assets/products/hornos-electricos/carousel-hornos-1.jpg";
+import SecondCarousel from "../../Assets/products/hornos-electricos/carousel-hornos-2.jpg";
+import ThirdCarousel from "../../Assets/products/hornos-electricos/carousel-hornos-3.jpg";
+import ProductHornos1 from "../../Assets/products/hornos-electricos/Productos/hornito-23lts-1.webp";
+import ProductHornos2 from "../../Assets/products/hornos-electricos/Productos/hornito-36lts-1.webp";
+import ProductHornos3 from "../../Assets/products/hornos-electricos/Productos/hornito-42lts-1.webp";
+import ProductHornos4 from "../../Assets/products/hornos-electricos/Productos/hornito-55lts-1.webp";
+import HeroHornosMob1 from "../../Assets/products/hornos-electricos/carousel-mob-hornos-1.jpg";
+import HeroHornosMob2 from "../../Assets/products/hornos-electricos/carousel-mob-hornos-2.jpg";
+import HeroHornosMob3 from "../../Assets/products/hornos-electricos/carousel-mob-hornos-3.jpg";
 
-const Hornos = () => {
+function HornosElectricos() {
   return (
-   <><div>Hornos</div></>
-  );
-};
+    <>
+      <div className="product-hero-cont"></div>
 
-export default Hornos;
+      {viewportWidth > 600 ? <></> : <></>}
+    </>
+  );
+}
+
+export default HornosElectricos;
