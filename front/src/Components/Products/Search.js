265b0d49b925449ebe40223ec06c6a83fb92caa8
pascualmanuel <manuelnpascual@gmail.com>
2025-01-15 12:54:24 -0300

    SearchBar Starting -  #007100

--- front/src/Components/Products/Search.js
+++ front/src/Components/Products/Search.js
@@ -1,11 +1,21 @@
-import React from "react";
+import "./Product.css";
 
 const Search = () => {
   return (
     <>
-      <div>Search</div>
+      <div className="form-container" ref={searchDiv}>
+        <div className="input-cont">
+          <input
+            placeholder="Type to search..."
+            value={searchTerm}
+            onChange={handleSearch}
+            type="text"
+          />
+        </div>
+
+        {searchTerm.length > 0 && <></>}
+      </div>
     </>
   );
 };
-
 export default Search;
