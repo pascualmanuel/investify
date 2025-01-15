81215200a99f6bbf3b35d7d108236f797570b852
pascualmanuel <manuelnpascual@gmail.com>
2025-01-15 12:54:42 -0300

    SearchBar States&Imports -  #007100

--- front/src/Components/Products/Search.js
+++ front/src/Components/Products/Search.js
@@ -1,6 +1,18 @@
 import "./Product.css";
+import Seatch from "./Search";
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Lupita } from "../../Assets/lupita.svg";
 
 const Search = () => {
+  const [data, setData] = useState([]);
+  const [keyword, setKeyword] = useState("");
+  const [newData, setNewData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+
+
   return (
     <>
       <div className="form-container" ref={searchDiv}>
