3a03eaaf86a8936d6886e434c2c687863a6b7dc0
pascualmanuel <manuelnpascual@gmail.com>
2025-01-22 16:16:18 -0300

    BannersChanges  #91242

--- front/src/Components/Products/Banners.jsx
+++ front/src/Components/Products/Banners.jsx
@@ -36,34 +36,7 @@ function Banners() {
       <section className="micro-banner-cont-hpt"></section>
       <section className="micro-banner-cont-hpt-mob"></section>
 
-      <section className="freezer-banner-cont">
-        <div className="micro-banner-left" id="freezer-img"></div>
-        <div className="micro-banner-right">
-          <p className="micro-banner-sub">FREEZER </p>
-          <h3 className="micro-banner-title">
-            un freezer <br></br>para cada hogar
-          </h3>
-          <p className="micro-banner-p">
-            Tus alimentos refrigerados de forma <br className="br-500" />
-            ecológica
-            <br className="br-banner-micro" />e inteligente.
-          </p>
-          <div className="cta-banner-cont">
-            <a href="" className="cta-tienda" style={{ marginLeft: "0px" }}>
-              Ver en tienda
-            </a>
-            <Link
-              to="/freezers"
-              onClick={() => {
-                window.scroll(0, 0);
-              }}
-              className="cta-banner"
-            >
-              Más info
-            </Link>
-          </div>
-        </div>
-      </section>
+
 
       <div className="pre-footer-back">
         <div className="pre-footer">
