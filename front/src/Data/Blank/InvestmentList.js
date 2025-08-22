433e9965a173dbe0882feca52f3bdee28dfb0dd5
pascualmanuel <manuelnpascual@gmail.com>
2025-08-22 13:06:40 +0200

    inv ilist

---
+++ front/src/Data/Blank/InvestmentList.js
@@ -0,0 +1,17 @@
+// src/components/InvestmentList.js
+import React from "react";
+
+const InvestmentList = ({ investments }) => {
+  return (
+    <ul>
+      {investments.map((investment, index) => (
+        <li key={index}>
+          {investment.crypto}: ${investment.amount.toFixed(2)} comprados el{" "}
+          {investment.date} a ${investment.price.toFixed(2)} cada uno
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default InvestmentList;
